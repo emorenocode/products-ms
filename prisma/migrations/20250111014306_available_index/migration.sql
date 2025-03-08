2fa8d0bd92686907afbc2b1afec933ff5d1b16f5
Enrique Moreno <emorenocode@gmail.com>
2025-03-08 18:21:07 -0500

    Create dockerfile

--- prisma/migrations/20250111014306_available_index/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "product_available_idx" ON "product"("available");
--- prisma/migrations/20250308231528_init/migration.sql
+++ prisma/migrations/20250308231528_init/migration.sql
@@ -3,6 +3,10 @@ CREATE TABLE "product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- CreateIndex
+CREATE INDEX "product_available_idx" ON "product"("available");
